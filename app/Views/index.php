c21301ed41db9d8351591cf638af260ace0b7890
nileshdabhivedaha <nilesh.d@vedaha.com>
2021-09-12 11:56:27 +0530

    first commit

---
+++ app/Views/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Codeigniter 4 Google Bar & Line Charts Example - XpertPhp</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="mb-5 mt-5">
+			<div id="GoogleLineChart" style="height: 400px; width: 100%"></div>
+		</div>
+		<div class="mb-5">
+			<div id="GoogleBarChart" style="height: 400px; width: 100%"></div>
+		</div>
+	</div>
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<script>
+		google.charts.load('current', {'packages':['corechart', 'bar']});
+		google.charts.setOnLoadCallback(drawLineChart);
+		google.charts.setOnLoadCallback(drawBarChart);
+
+		// Line Chart
+		function drawLineChart() {
+		var data = google.visualization.arrayToDataTable([
+			['Day', 'Products Count'],
+				<?php 
+					foreach ($productData as $row){
+					   echo "['".$row['day']."',".$row['sell']."],";
+				} ?>
+		]);
+
+		var options = {
+			title: 'Line chart product sell wise',
+			curveType: 'function',
+			legend: {
+				position: 'top'
+			}
+		};
+		var chart = new google.visualization.LineChart(document.getElementById('GoogleLineChart'));
+		chart.draw(data, options);
+		}
+
+
+		// Bar Chart
+		google.charts.setOnLoadCallback(showBarChart);
+		function drawBarChart() {
+		var data = google.visualization.arrayToDataTable([
+			['Day', 'Products Count'], 
+				<?php 
+					foreach ($productData as $row){
+					   echo "['".$row['day']."',".$row['sell']."],";
+					}
+				?>
+		]);
+		var options = {
+			title: ' Bar chart products sell wise',
+			is3D: true,
+		};
+		var chart = new google.visualization.BarChart(document.getElementById('GoogleBarChart'));
+		chart.draw(data, options);
+		}
+	</script>
+</body>
+</html>
